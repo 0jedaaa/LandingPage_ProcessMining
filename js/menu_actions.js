cceb79a27f79066720b88a54cebe135983ca98d6
JonatasOjeda <jonatas.ferreira@t2cconsultoria.com.br>
2022-04-28 10:51:40 -0300

    First Commit

---
+++ js/menu_actions.js
@@ -0,0 +1,19 @@
+const $menu = document.querySelector('.menu')
+const $btnMenuOpen = document.querySelector('.btnMenu_open')
+const $btnMenuClose = document.querySelector('.btnMenu_close')
+const navItem = document.querySelectorAll('li'); 
+
+$btnMenuOpen.addEventListener('click', function(){
+ $menu.classList.add('menu_open')
+})
+
+$btnMenuClose.addEventListener('click', function(){
+    $menu.classList.remove('menu_open')
+   })
+
+navItem.forEach(li =>{
+    li.addEventListener('click', function(){
+        navItem.forEach(btn => btn.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
